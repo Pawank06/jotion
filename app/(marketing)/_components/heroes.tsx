e9d2fcfd7252a6d68dc7254dc8b5aa0af1cf1b9a
PawanK06 <pawan06kumar2003@gmail.com>
2024-09-03 21:08:24 +0530

    add heroes section

--- app/(marketing)/_components/heroes.tsx
+++ app/(marketing)/_components/heroes.tsx
@@ -2,8 +2,25 @@ import Image from "next/image"
 
 const Heroes = () => {
   return (
-    <div>
-        
+    <div className="flex flex-col items-center justify-center max-w-5xl">
+        <div className="flex items-center">
+            <div className="relative w-[300px] h-[300px] sm:w-[350px] sm:h-[350px] md:w-[400px]">
+                <Image 
+                src="/documents.png"
+                alt="Documents"
+                fill
+                className="object-contain"
+                />
+            </div>
+            <div className="relative h-[400px] w-[400px] hidden md:block">
+                <Image
+                src="/reading.png"
+                fill
+                alt="Reading"
+                className="object-contain"
+                />
+            </div>
+        </div>
     </div>
   )
 }
